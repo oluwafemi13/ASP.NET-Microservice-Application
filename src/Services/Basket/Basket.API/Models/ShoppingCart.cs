7abc186e65d59b6a69ed138b8f038701a2b66730
oluwafemi13 <sundayjabikem@gmail.com>
2022-11-09 22:42:40 +0100

    basket Api using Redis Database

---
+++ src/Services/Basket/Basket.API/Models/ShoppingCart.cs
@@ -0,0 +1,32 @@
+﻿namespace Basket.API.Models
+{
+    public class ShoppingCart
+    {
+        public string UserName { get; set; }
+
+        public List<ShoppingCartItems> items { get; set; } = new List<ShoppingCartItems>();
+
+
+        public ShoppingCart(string userName)
+        {
+            UserName = userName;
+        }
+
+        public decimal TotalPrice
+        {
+
+            get
+            {
+                decimal totalPrice = 0;
+                foreach (var item in items)
+                {
+                    totalPrice = item.Price * item.Quantity;
+                    
+                }
+                return totalPrice;
+            }
+
+        }
+
+    }
+}
---
+++ src/Services/Basket/Basket.API/Models/ShoppingCartItems.cs
@@ -0,0 +1,14 @@
+﻿namespace Basket.API.Models
+{
+    public class ShoppingCartItems
+    {
+        public string ProductName { get; set; }
+
+        public string ProductId { get; set; }
+
+        public decimal Price { get; set; }
+        //public string color { get; set; }
+        public int Quantity { get; set; }
+
+    }
+}
