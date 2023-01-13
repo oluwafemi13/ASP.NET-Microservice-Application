fee8b4a31c71e1dfdf708aacb11b5f3c907eeeac
IKECHUKWU <sundayjabikem@gmail.com>
2023-01-13 14:18:44 +0100

    made changes to the delete command

--- src/Services/Ordering/Ordering.Application/Features/Orders/Commands/CheckoutOrder/CheckoutOrderCommand.cs
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Commands/CheckoutOrder/CheckoutOrderCommand.cs
@@ -9,6 +9,7 @@ namespace Ordering.Application.Features.Orders.Commands.CheckoutOrder
 {
     public class CheckoutOrderCommand: IRequest<int>
     {
+        
         public string UserName { get; set; }
         public decimal TotalPrice { get; set; }
         // public DateTime OrderDate { get; set; }
