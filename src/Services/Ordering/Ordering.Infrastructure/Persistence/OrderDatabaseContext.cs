9d369a1ef72c7c6bae6a0d86202112c0cc151662
IKECHUKWU <sundayjabikem@gmail.com>
2023-01-16 21:57:59 +0100

    infrastructure Layer

---
+++ src/Services/Ordering/Ordering.Infrastructure/Persistence/OrderDatabaseContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Ordering.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Infrastructure.Persistence
+{
+    public class OrderDatabaseContext: DbContext
+    {
+        public OrderDatabaseContext():base()
+        {
+
+        }
+        public DbSet<Order> Orders { get; set; }
+    }
+}
