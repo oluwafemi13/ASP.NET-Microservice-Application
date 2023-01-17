5bce396d1fbe65b556fabac93ddb57412a1824e3
IKECHUKWU <sundayjabikem@gmail.com>
2023-01-17 15:23:04 +0100

    Repository implementation in the infrastructure Layer

--- src/Services/Ordering/Ordering.Infrastructure/Persistence/OrderDatabaseContext.cs
+++ src/Services/Ordering/Ordering.Infrastructure/Persistence/OrderDatabaseContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Ordering.Domain.Common;
 using Ordering.Domain.Entities;
 using System;
 using System.Collections.Generic;
@@ -10,10 +11,30 @@ namespace Ordering.Infrastructure.Persistence
 {
     public class OrderDatabaseContext: DbContext
     {
-        public OrderDatabaseContext():base()
+        public OrderDatabaseContext(DbContextOptions<OrderDatabaseContext> options):base(options)
         {
 
         }
         public DbSet<Order> Orders { get; set; }
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = new CancellationToken())
+        {
+            var order = new Order();
+            foreach (var entry in ChangeTracker.Entries<EntityBase>())
+            {
+                switch (entry.State)
+                {
+                    case EntityState.Added:
+                        entry.Entity.CreatedDate = DateTime.Now;
+                        entry.Entity.CreatedBy = order.UserName;
+                        break;
+                    case EntityState.Modified:
+                        entry.Entity.LastModifiedDate = DateTime.Now;
+                        entry.Entity.LastModifiedBy = order.UserName;
+                        break;
+                }
+            }
+            return base.SaveChangesAsync(cancellationToken);
+        }
     }
 }
---
+++ src/Services/Ordering/Ordering.Infrastructure/Persistence/OrderDatabaseContextSeed.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Logging;
+using Ordering.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Infrastructure.Persistence
+{
+    public class OrderDatabaseContextSeed
+    {
+        public static async Task SeedAsync(OrderDatabaseContext orderContext, ILogger<OrderDatabaseContextSeed> logger)
+        {
+            if (!orderContext.Orders.Any())
+            {
+                orderContext.Orders.AddRange(GetPreconfiguredOrders());
+                await orderContext.SaveChangesAsync();
+                logger.LogInformation("Seed database associated with context {DbContextName}", typeof(OrderDatabaseContext).Name);
+            }
+        }
+
+        private static IEnumerable<Order> GetPreconfiguredOrders()
+        {
+            return new List<Order>
+            {
+                new Order() {UserName = "Anonymous", FirstName = "Femi", LastName = "Iwuchukwu", EmailAddress = "jabikem@gmail.com", AddressLine = "Ikorodu", Country = "Nigeria", TotalPrice = 455 }
+            };
+        }
+    }
+}
