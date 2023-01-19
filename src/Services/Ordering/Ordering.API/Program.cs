cde7282bdb258fd3291603b8fb0c0f27d24dda32
IKECHUKWU <sundayjabikem@gmail.com>
2023-01-19 15:37:13 +0100

    update

--- src/Services/Ordering/Ordering.API/Program.cs
+++ src/Services/Ordering/Ordering.API/Program.cs
@@ -7,28 +7,20 @@ using Ordering.Application.Contracts.Infrastructure.Interface;
 using Ordering.Application.Models.Email;
 using Ordering.Application.persistence.Interface;
 using Ordering.Infrastructure;
+using Microsoft.Extensions.Configuration;
 using Ordering.Infrastructure.Mail;
 using Ordering.Infrastructure.Persistence;
 using Ordering.Infrastructure.Repositories;
+using EllipticCurve;
 
 var builder = WebApplication.CreateBuilder(args);
-
 // Add Services to the container.
 
-builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
-builder.Services.AddDbContext<DatabaseContext>(options =>
-                options.UseSqlServer(configuration.GetConnectionString("OrderingConnectionString")));
-
-Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
-Services.AddScoped<IOrdeRepository, OrderRepository>();
-
-Services.Configure<EmailSettings>(c => configuration.GetSection("EmailSettings"));
-Services.AddTransient<IEmailService, EmailService>();
 builder.Services.AddApplicationServices();
+builder.Services.AddInfrastructureServices(builder.Configuration);
 
 var app = builder.Build();
 
